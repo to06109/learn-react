fdd8931b42735da5d5846786a3247060390147e4
Yesong Choi <to06109@naver.com>
2023-02-15 10:00:55 +0900

    learn. React 상태 업데이트

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,19 +1,46 @@
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    // component state
+    this.state = {
+      headline: 'React Application',
+    }
+    // component methods binding `this`
+    this.handleChangeHeadline = this.handleChangeHeadline.bind(this)
+  }
+
+  state = {
+    headline: 'React Application',
+  }
+
+  // this.handleChangeHeadline
+  handleChangeHeadline() {
+    // this는 과연 컴포넌트의 인스턴스(엘리먼트)인가??
+    console.log(this) // App {}
+    this.setState({
+      headline: 'NEW HEADLINE!!',
+    }) //상태 변경 시도 -> React UI 업데이트(재조정 알고리즘)
+  }
+
   render() {
     return (
       <div data-component="App">
-        <h1>React Application</h1>
+        <h1>{this.state.headline}</h1>
+        {/* React의 JSX(React 엘리먼트)에 연결한 이벤트 리스너(함수)는 언제 실행되는가? */}
+        <button type="button" onClick={this.handleChangeHeadline}>
+          헤드라인 변경
+        </button>
       </div>
-    );
+    )
   }
 }
 
 /* -------------------------------------------------------------------------- */
 
-const reactDomRoot = ReactDOM.createRoot(document.getElementById('root'));
+const reactDomRoot = ReactDOM.createRoot(document.getElementById('root'))
 
 reactDomRoot.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
