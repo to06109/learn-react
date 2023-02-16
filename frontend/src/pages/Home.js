89ba6dc6cb28219ddd30dcbb8cdd3b4ad5a68151
Yesong Choi <to06109@naver.com>
2023-02-16 10:30:21 +0900

    learn. 리스트 렌더링(map 이용)

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -15,37 +15,35 @@ class Home extends React.Component {
         image:
           'https://contents.lotteon.com/itemimage/_v105312/LF/14/37/17/3_/0/LF1437173_0_2.jpg/dims/resizef/720X720',
       },
+      ``,
     ],
   }
 
   render() {
     const { products } = this.state
 
-    console.log(products[0])
-    console.log(products[1])
+    const figcaptionStyle = {
+      display: 'flex',
+      gap: 12,
+    }
 
     return (
       <React.Fragment>
         <h2>리스트 렌더링</h2>
         <ul>
-          <li>
-            <figure data-id={products[0].id}>
-              <img src={products[0].image} height={100} alt />
-              <figcaption>
-                <span>{products[0].name}</span>
-                <span>{products[0].price}</span>
-              </figcaption>
-            </figure>
-          </li>
-          <li>
-            <figure data-id={products[1].id}>
-              <img src={products[1].image} height={100} alt />
-              <figcaption>
-                <span>{products[1].name}</span>
-                <span>{products[1].price}</span>
-              </figcaption>
-            </figure>
-          </li>
+          {products.map(({id, name, price, image}) => {
+            return (
+              <li key={id}>
+                <figure data-id={id}>
+                  <img src={image} height={100} alt="" />
+                  <figcaption style={figcaptionStyle}>
+                    <span>{name}</span>
+                    <span>{price}</span>
+                  </figcaption>
+                </figure>
+              </li>
+            )
+          })}
         </ul>
       </React.Fragment>
     )
