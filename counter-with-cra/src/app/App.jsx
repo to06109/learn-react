5bf6097cc0d6c288d208f2cc9fb963c885d2670e
Yesong Choi <to06109@naver.com>
2023-02-21 03:25:27 +0900

    learn. 카운터 클래스 컴포넌트

--- counter-with-cra/src/app/App.jsx
+++ counter-with-cra/src/app/App.jsx
@@ -1,27 +1,13 @@
-// 이미지 에셋 불러오기
-import logo from '../assets/logo.svg'
 import './App.css'
+import Counter from '../components/Counter'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="React" />
-        <p>
-          <code>src/App.js</code> 파일을 수정하면{' '}
-          <abbr title="Hto Module Replacement">HMR</abbr>, Live Reload 됩니다.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React 배우러 가즈아
-        </a>
-      </header>
+      <Counter count={1} />
+      <Counter count={3} />
+      <Counter count={9} />
     </div>
   )
 }
-
 export default App
