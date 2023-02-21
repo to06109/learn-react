c02351e547cd23c26cf9921c1432ffb6823f6b1b
Yesong Choi <to06109@naver.com>
2023-02-21 16:32:35 +0900

    learn. 로딩 스피너 with SVGR and A11Y

--- counter-with-cra/src/components/LifeCycle/LifeCycle.jsx
+++ counter-with-cra/src/components/LifeCycle/LifeCycle.jsx
@@ -1,56 +1,63 @@
 import React from 'react'
+import spinnerURL, { ReactComponent as Spinner } from '../../assets/spinner.svg'
 
-// Component's Life Cycle (컴포넌트의 생명주기)
-// 탄생 (constructor: 1번) -> (render: N번)
-// 성장
-// 죽음
+console.log(spinnerURL)
+console.log(Spinner)
+let API_ENDPOINT = 'https://randomuser.me/api/?results=5'
 
 class LifeCycle extends React.Component {
   /* render 단계 ------------------------------------------------------------ */
 
   state = {
-    message: 'loading...',
+    // 로딩 중인가요? (데이터 패치 요청했나요?)
+    isLoading: false,
+    // 통신 결과가 도착했나요?
+    data: [],
+    // 오류는 어떤 정보(상태 코드, 메시지)를 가지고 있나요?
+    error: null,
+    // 혹시 오류가 발생했나요?
+    // hasError: false,
   }
 
   render() {
-    // console.log(document.querySelector('.LifeCycle')) // null
+    if (this.state.isLoading) {
+      return (
+        <div
+          role="alert"
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            minHeight: '100vh',
+          }}
+        >
+          <Spinner title='로딩 중...'/>
+        </div>
+      )
+    }
     return (
       <>
-        <div>
-          <div className="LifeCycle" tabIndex={0}>
-            컴포넌트의 생명 주기
-          </div>
-          <input
-            id="select-me"
-            type="text"
-            placeholder="위에 요소를 클릭하면 초점이 내게 와요~"
-            aria-label="나를 선택하세요"
-          />
-        </div>
-
         <button
           type="button"
-          onClick={() => this.setState({ message: 'updated content' })}
+          onClick={() => this.fetchRandomPeople(API_ENDPOINT)}
         >
-          {this.state.message}
+          다양한 사람들을 보고 싶어요!
         </button>
+
+        {/* <ul>
+          <li></li>
+        </ul> */}
       </>
     )
-
-    // return React.createElement(
-    //   'div',
-    //   { className: 'LifeCycle' },
-    //   '컴포넌트의 생명 주기'
-    // );
   }
 
   /* commit 단계 ------------------------------------------------------------ */
-
-  // - 실제 DOM에 접근, 조작 (DOM 스크립트 또는 Vanilla 스크립트)
-
-  // promise (.then, .catch)
-  // async function (await, try ~ cache)
   async fetchRandomPeople(endpoint) {
+    // Random User API 서버에 요청
+    this.setState({
+      isLoading: true,
+    })
+
     const response = await fetch(endpoint)
 
     if (!response.ok) {
@@ -66,41 +73,40 @@ class LifeCycle extends React.Component {
   // 컴포넌트가 마운트 된 이후
   // 마운트 (1회 실행)
   componentDidMount() {
-    // 사이드 이펙트 2번째 스토리
-    let API_ENDPOINT = 'https://randomuser.me/api/?results=5'
-
     // API 요청/응답
     // fetch API
-    this.fetchRandomPeople(API_ENDPOINT)
+    // this.fetchRandomPeople(API_ENDPOINT)
 
     // 바닐라 프로그래밍 (React가 아닌 것 === 사이드 이펙트)
     // 명령형 프로그래밍
     const lifecycleElement = document.querySelector('.LifeCycle')
     const selectMeInput = document.getElementById('select-me')
 
-    // click : mouse event (a11y)
-    // focusable element
-    lifecycleElement.addEventListener('click', (e) => {
-      e.target.style.cssText = `
+    if (lifecycleElement) {
+      // click : mouse event (a11y)
+      // focusable element
+      lifecycleElement.addEventListener('click', (e) => {
+        e.target.style.cssText = `
         background: skyblue;
         color: darkblue;
         font-size: 3rem;
         padding: 20px;
       `
 
-      // 문서의 input 요소를 찾아서 초점을 이동
-      setTimeout(() => {
-        selectMeInput.value = '나! 선택받았어~~'
-        selectMeInput.select()
-      }, 1500)
-    })
+        // 문서의 input 요소를 찾아서 초점을 이동
+        setTimeout(() => {
+          selectMeInput.value = '나! 선택받았어~~'
+          selectMeInput.select()
+        }, 1500)
+      })
 
-    lifecycleElement.addEventListener('keyup', (e) => {
-      // enter 눌렀을 때
-      if (e.key.toLowerCase().includes('enter')) {
-        lifecycleElement.click()
-      }
-    })
+      lifecycleElement.addEventListener('keyup', (e) => {
+        // enter 눌렀을 때
+        if (e.key.toLowerCase().includes('enter')) {
+          lifecycleElement.click()
+        }
+      })
+    }
   }
 
   // 컴포넌트가 업데이트 된 이후
