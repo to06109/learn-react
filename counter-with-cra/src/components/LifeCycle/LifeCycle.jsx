3b352e7142ed2a412da1c28606dc583f98adce29
Yesong Choi <to06109@naver.com>
2023-02-21 15:24:49 +0900

    learn. 웹 접근성 준수 - div 요소에 클릭 이벤트 걸지 말고 button 요소에 걸자

--- counter-with-cra/src/components/LifeCycle/LifeCycle.jsx
+++ counter-with-cra/src/components/LifeCycle/LifeCycle.jsx
@@ -13,13 +13,17 @@ class LifeCycle extends React.Component {
   }
 
   render() {
-    console.log(document.querySelector('.LifeCycle')) // null
+    // console.log(document.querySelector('.LifeCycle')) // null
     return (
       <>
-        <div className="LifeCycle">컴포넌트의 생명 주기</div>
+        <div className="LifeCycle" tabIndex={0}>
+          컴포넌트의 생명 주기
+        </div>
         <input
+          id="select-me"
           type="text"
           placeholder="위에 요소를 클릭하면 초점이 내게 와요~"
+          aria-label="나를 선택하세요"
         />
       </>
     )
@@ -36,14 +40,15 @@ class LifeCycle extends React.Component {
   // - 실제 DOM에 접근, 조작 (DOM 스크립트 또는 Vanilla 스크립트)
 
   // 컴포넌트가 마운트 된 이후
+  // 마운트 (1회 실행)
   componentDidMount() {
-    console.log('여기서는 실제 DOM에 접근이 가능해요')
-    // console.log(document.querySelector('.LifeCycle')) // <div className="LifeCycle">컴포넌트의 생명주기</div>
-
     // 바닐라 프로그래밍 (React가 아닌 것 === 사이드 이펙트)
     // 명령형 프로그래밍
     const lifecycleElement = document.querySelector('.LifeCycle')
+    const selectMeInput = document.getElementById('select-me')
 
+    // click : mouse event (a11y)
+    // focusable element
     lifecycleElement.addEventListener('click', (e) => {
       e.target.style.cssText = `
         background: skyblue;
@@ -51,6 +56,19 @@ class LifeCycle extends React.Component {
         font-size: 3rem;
         padding: 20px;
       `
+
+      // 문서의 input 요소를 찾아서 초점을 이동
+      setTimeout(() => {
+        selectMeInput.value = '나! 선택받았어~~'
+        selectMeInput.select()
+      }, 1500)
+    })
+
+    lifecycleElement.addEventListener('keyup', (e) => {
+      // enter 눌렀을 때
+      if (e.key.toLowerCase().includes('enter')) {
+        lifecycleElement.click()
+      }
     })
   }
 }
