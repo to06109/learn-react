8449062a9875ced38525c8a051d9e08ac3f5d8de
Yesong Choi <to06109@naver.com>
2023-02-21 11:20:00 +0900

    learn. 함수 컴포넌트 props + 기본 매개변수 설정

--- counter-with-cra/src/components/Counter/Counter.function.jsx
+++ counter-with-cra/src/components/Counter/Counter.function.jsx
@@ -1,35 +1,18 @@
 import { useState } from 'react'
 
-function Counter(props) {
-  // 함수 안에 지역 변수 선언
-  // let count = 1;
-  // let defaultProps = {
-  //   count: 1,
-  //   min: 1,
-  //   max: 10,
-  //   step: 1,
-  // };
-
-  // React Hook
-  // 상태 관리 React.useState()
-  // 객체 { prop1, prop2 }
-  // 배열 [ state, setStaet ]
-  let [count, setCount] = useState(props.count)
+function Counter({ min = 1, count: initialCount = 1, max = 10, step = 1 }) {
+  let [count, setCount] = useState(initialCount)
 
   // 함수 안에 이벤트 청취 함수를 작성합니다.
   const handleIncrement = () => {
-    // console.log('카운트 값 증가');
-    // count++; // 수정(mutation)
-    setCount(count + props.step)
+    setCount(count + step)
   }
 
   // only class component
   // lifecycle methods
 
   const handleDecrement = () => {
-    // console.log('카운트 값 감소');
-    // count--;
-    setCount(count - props.step)
+    setCount(count - step)
   }
 
   return (
@@ -44,14 +27,6 @@ function Counter(props) {
       </button>
     </div>
   )
-}
-
-// like class's static member
-Counter.defaultProps = {
-  count: 1,
-  min: 1,
-  max: 10,
-  step: 1,
 }
 
 export default Counter
