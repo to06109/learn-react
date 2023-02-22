78a85330f98c128974848ec7d32cb2b8b2de4bbe
Yesong Choi <to06109@naver.com>
2023-02-22 09:12:54 +0900

    refactor. LifeCycle 코드 정리

--- counter-with-cra/src/components/LifeCycle/LifeCycle.jsx
+++ counter-with-cra/src/components/LifeCycle/LifeCycle.jsx
@@ -1,22 +1,17 @@
 import React from 'react'
+import styles from './LifeCycle.module.css'
 import spinnerURL, { ReactComponent as Spinner } from '../../assets/spinner.svg'
 
-console.log(spinnerURL)
-console.log(Spinner)
 let API_ENDPOINT = 'https://randomuser.me/api/?results=5'
 
 class LifeCycle extends React.Component {
   /* render 단계 ------------------------------------------------------------ */
 
   state = {
-    // 로딩 중인가요? (데이터 패치 요청했나요?)
     isLoading: false,
-    // 통신 결과가 도착했나요?
     data: [],
-    // 오류는 어떤 정보(상태 코드, 메시지)를 가지고 있나요?
     error: null,
-    // 혹시 오류가 발생했나요?
-    // hasError: false,
+    hasError: false,
   }
 
   render() {
@@ -36,7 +31,7 @@ class LifeCycle extends React.Component {
       )
     }
     return (
-      <>
+      <div className={styles.container}>
         <button
           type="button"
           onClick={() => this.fetchRandomPeople(API_ENDPOINT)}
@@ -49,27 +44,19 @@ class LifeCycle extends React.Component {
             <li key={user.cell}>{user.email}</li>
           ))}
         </ul>
-      </>
+      </div>
     )
   }
 
   /* commit 단계 ------------------------------------------------------------ */
-
-  // React + Firebase 클라우드Backend (Serverless)
-  // - 인증(회원가입, 로그인, SNS 로그인)
-  // - 파이어스토어 (데이터베이스: JSON 파일 구조)
-  // - 스토리지 (이미지, 에셋 업로드 -> URL값 반환)
-  // - 호스팅 (서비스, FTP -> 무료 웹 호스팅)
-
+  // Random User API 서버에 요청
   async fetchRandomPeople(endpoint) {
-    // Random User API 서버에 요청
     try {
       const response = await fetch(endpoint)
       const data = await response.json()
       this.setState({
         data: data.results,
       })
-      console.log(this.state.data)
     } catch (error) {
       this.setState({
         error: {
