72d6f94998834d991cff9943db4dbbbde3ae90f1
Yesong Choi <to06109@naver.com>
2023-02-22 13:36:02 +0900

    learn. CRA 기능 - 절대 경로 (jsconfig.json) - CSS 모듈 (*.module.css)

--- counter-with-cra/src/app/App.jsx
+++ counter-with-cra/src/app/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
-import './App.css'
-import { CounterFunction, CounterClass, Button, LifeCycle, API_ENDPOINT } from 'components'
+import classes from './App.module.css'
+import {
+  CounterFunction,
+  CounterClass,
+  Button,
+  LifeCycle,
+  API_ENDPOINT,
+} from 'components'
 import { ReactComponent as ReactLogo } from 'assets/logo.svg'
 
 function renderComponents(isVisible) {
@@ -20,32 +26,14 @@ function renderComponents(isVisible) {
   }
 }
 
-// 함수 컴포넌트
-// React v16.8 (2019)
-// - 상태 관리 React.useState
-// - 사이드 이펙트 관리 React.useEffect
 function App() {
-  // 관심사의 분리
-  // 앱의 로딩 상태
-  // 로딩 이후, 데이터 관리
-  // 로딩 이후, 통신 실패 -> 오류 처리
-
-  // component state = {isLoading, error, data} 클래스 컴포넌트는 하나로 묶어서 관리
-  // 함수 컴포넌트는 하나씩 관리
   const [isLoading, setIsLoading] = React.useState(true)
   const [error, setError] = React.useState(null)
   const [data, setData] = React.useState('')
 
   // 사이드 이펙트 처리 훅
   React.useEffect(() => {
-    // console.log('componentDidMount')
-
-    // fecth data
-    // async function???????
-    // 이펙트 함수 안에서 비동기 함수를 작성하는 건 가능하다
     async function fetchData() {
-      // const data = await (await fetch(API_ENDPOINT)).json();
-
       try {
         const response = await fetch(API_ENDPOINT)
         const data = await response.json()
@@ -61,16 +49,12 @@ function App() {
   }, [])
 
   React.useEffect(() => {
-    // 만약 여기서 네트워느 요청 / 응답을 처리한다면?
-    // 데이터 요청 -> 상태 업데이트 -> 리렌더링
-    // ...
     console.log('componentDidUpdate')
   })
 
   // ----------------------------------------------------------------------------
 
   // React 상태 관리 훅(Hook)
-  // 상태 변수(state variable)
   const [isVisibleComponents, updateIsVisibleComponents] = React.useState(false)
 
   // 함수 안에 이벤트 핸들러 작성
@@ -78,10 +62,9 @@ function App() {
     updateIsVisibleComponents(!isVisibleComponents)
   }
 
-  // class's render method
   return (
-    <div className="App">
-      <ReactLogo title="리액트 로고" />
+    <div className={classes.container}>
+      <ReactLogo className={classes.logo} title="리액트 로고" />
       <button type="button" onClick={handleToggleVisible}>
         {isVisibleComponents.toString()}
       </button>
