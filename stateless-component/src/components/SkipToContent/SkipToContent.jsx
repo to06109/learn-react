36728147786ffa58a0594adb4fcbfd5f6f77af7f
Yesong Choi <to06109@naver.com>
2023-02-23 14:02:31 +0900

    refactor. SkipToContent, A11yHidden 컴포넌트 코드 정리

--- stateless-component/src/components/SkipToContent/SkipToContent.jsx
+++ stateless-component/src/components/SkipToContent/SkipToContent.jsx
@@ -1,29 +1,16 @@
 import { useEffect, useRef } from 'react'
-import { A11yHidden } from '../A11yHidden/A11yHidden'
-
+import { A11yHidden } from '@/components'
 export function SkipToContent({ to, ...restProps }) {
-  // DOM요소가 없을 때는 초기값 null
-  const skipToContentRef = useRef(null) // {current: null}
+  const skipToContentRef = useRef(null) // { current: null }
 
   let targetElement = null
 
-  console.log(skipToContentRef)
-
-  // 사이드 이펙트
-  // 명령형 프로그래밍
   useEffect(() => {
-    // 복사(primitive) vs. 참조(object)
-
-    // 실제 DOM 요소(객체) 접근(참조)
     targetElement = document.querySelector(to) // null
 
     if (targetElement) {
-      // 실제 DOM 요소 조작 -> 사이드 이펙트이기 때문에 useEffect 사용
       targetElement.setAttribute('tabindex', -1)
 
-      // 이벤트 핸들링
-      // React의 방식(useRef)
-      // skipToContentRef.current
       skipToContentRef.current.addEventListener('click', (e) => {
         e.preventDefault()
         targetElement && targetElement.focus()
@@ -31,26 +18,13 @@ export function SkipToContent({ to, ...restProps }) {
     }
   }, [])
 
-  // const handleJumpToArea = (e) => {
-  //   // 사이드 이펙트
-  //   e.preventDefault();
-
-  //   if (targetElement) {
-  //     targetElement.focus();
-  //   }
-  // };
-
-  // React 요소 ({ref, key, props, type}) → ReactDOM 렌더링 → 실제 DOM
   return (
-    <>
-      <A11yHidden
-        ref={skipToContentRef}
-        as="a"
-        focusable
-        href={to}
-        // onClick={handleJumpToArea}
-        {...restProps}
-      />
-    </>
+    <A11yHidden
+      ref={skipToContentRef}
+      as="a"
+      focusable
+      href={to}
+      {...restProps}
+    />
   )
 }
