5a1eb11f8dd71d577cfeee9b8675cff69a134ff9
Yesong Choi <to06109@naver.com>
2023-02-23 09:54:48 +0900

    learn. 상태가 없는 컴포넌트 설계 A11yHidden

--- stateless-component/src/App.jsx
+++ stateless-component/src/App.jsx
@@ -14,10 +14,25 @@ function App() {
         <Button secondary onClick={handleClick}>
           로그인
         </Button>
-        <h2>저장</h2>
+        <section>
+          <A11yHidden as="h2">접근성 준수한 제목</A11yHidden>
+          <p>
+            Lorem ipsum <a href="dolor"></a> sit amet consectetur adipisicing
+            elit. Laboriosam temporibus aperiam magni deserunt libero quos,
+            autem corporis nisi doloribus odit voluptatibus neque reprehenderit,
+            nesciunt vel in facilis quam? Amet, autem.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+            temporibus aperiam magni deserunt libero quos, autem corporis nisi
+            doloribus odit voluptatibus neque reprehenderit, nesciunt vel in
+            facilis quam? Amet, autem.
+          </p>
+        </section>
+        {/*         
         <A11yHidden as="h2">저장</A11yHidden>
         <A11yHidden as="a">저장</A11yHidden>
-        <A11yHidden as="figcaption">저장</A11yHidden>
+        <A11yHidden as="figcaption">저장</A11yHidden> */}
       </div>
     </div>
   )
--- stateless-component/src/components/A11yHidden/A11yHidden.jsx
+++ stateless-component/src/components/A11yHidden/A11yHidden.jsx
@@ -2,24 +2,13 @@
 import classes from './A11yHidden.module.css'
 
 export const A11yHidden = ({
-  // 컴포넌트 이름 설정
-  // React는 브라우저가 인식할 수 없는 HTML 표준 요소가 아닌 경우
-  // 컴포넌트 이름의 첫글자를 대문자로 구분한다.
-  // React를 속여(trick) 보자!
-
-  // 비 표준 속성이므로 restProps에서 제외해야 한다.
-  focusable,
-  as: ComponentName,
-
+  as: ComponentName = 'span',
+  focusable = false,
+  className = '',
   ...restProps
 }) => {
-  // as는 렌더링 할 컴포넌트 요소의 (태그) 이름
-  // : 뒤에 쓰는 이름이 별칭(지역 변수), 마치 리액트 컴포넌트 이름인 것처럼 트릭!
-
-  // h 함수는 React.createElement의 별칭 함수입니다.
-  // React.createElement(type[, props][, ...children])
-  // h(type[, props][, ...children])
-  // h(태그이름, props 객체(children 포함))
-  // return h(as, restProps);
-  return <ComponentName className={classes.srOnly} {...restProps} />
+  // 변수(문자 값) 변수(문자 값)
+  // `${개발ㄹ자의 클래스이름} ${사용자의 클래스이름}`
+  const combinedClassName = `${classes.srOnly} ${className}`.trim()
+  return <ComponentName className={combinedClassName} {...restProps} />
 }
