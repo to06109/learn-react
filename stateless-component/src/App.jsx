77dd3a41eebeb04d1769e678363ba027e0d5fc31
Yesong Choi <to06109@naver.com>
2023-02-23 13:55:35 +0900

    learn. 고차 컴포넌트 & ref 전달하기

--- stateless-component/src/App.jsx
+++ stateless-component/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import classes from './App.module.css'
-import { Button, A11yHidden, Nav } from './components'
+import { Button, A11yHidden, Nav, SkipToContent } from './components'
 
 function App() {
   const [navList] = useState([
@@ -13,39 +13,59 @@ function App() {
 
   return (
     <div className={classes.container}>
-      {/* <SkipToContent /> */}
+      <SkipToContent to="#snack">과자</SkipToContent>
+
       <Nav as="h3" headline="상품 목록" list={navList} />
+
       <h2 className={classes.headline}>Button 컴포넌트(stateless)</h2>
       <div role="group" className={classes.buttonGroup}>
-        <Button onClick={handleClick}>회원가입</Button>
+        <Button>회원가입</Button>
         <Button secondary>로그인</Button>
-        <section id="snack">
-          <A11yHidden as="h2" focusable>
-            맛있는 과자~
-          </A11yHidden>
-          <p>
-            <A11yHidden as="a" href="#invisible" focusable>
-              Lorem ipsum
-            </A11yHidden>{' '}
-            <a href="dolor">dolor</a> sit amet consectetur adipisicing elit.
-            Laboriosam temporibus aperiam magni deserunt libero quos, autem
-            corporis nisi doloribus odit voluptatibus neque reprehenderit,
-            nesciunt vel in facilis quam? Amet, autem.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-            temporibus aperiam magni deserunt libero quos, autem corporis nisi
-            doloribus odit voluptatibus neque reprehenderit, nesciunt vel in
-            facilis quam? Amet, autem.
-          </p>
-        </section>
-        {/*         
-        <A11yHidden as="h2">저장</A11yHidden>
-        <A11yHidden as="a">저장</A11yHidden>
-        <A11yHidden as="figcaption">저장</A11yHidden> */}
       </div>
+      <section id="snack" tabIndex={0}>
+        <A11yHidden as="h2" focusable>
+          맛있는 과자~
+        </A11yHidden>
+        <p>
+          <A11yHidden as="a" href="#invisible" focusable>
+            Lorem ipsum
+          </A11yHidden>
+          <a href="#dolor">dolor</a>, sit amet consectetur adipisicing elit.
+          Quis suscipit ratione maxime velit, distinctio cupiditate dignissimos
+          fugit culpa, necessitatibus, quidem obcaecati perspiciatis veritatis.
+          Atque quidem sed nisi maxime aliquid eos.
+        </p>
+        <p>
+          Dolorem adipisci nihil et vitae possimus ipsa dolorum voluptates
+          exercitationem nemo aperiam deserunt dolor aspernatur, nostrum
+          expedita temporibus veniam, suscipit recusandae sint necessitatibus
+          fugiat voluptatibus accusantium tempore accusamus nisi. Culpa?
+        </p>
+        <p>
+          Expedita, rerum. Voluptatem impedit nisi numquam dolorum ad unde, quis
+          illum odio voluptatibus eveniet tempora explicabo distinctio aliquid
+          vel ullam beatae quos neque laudantium esse voluptatum minima deleniti
+          voluptate. Libero.
+        </p>
+        <p>
+          Quam ipsam repellendus magnam repellat cumque. Dolores sit, quisquam a
+          ex error eos. Ducimus pariatur eaque officiis, debitis quo inventore
+          doloremque fugit iste rem necessitatibus! A possimus quidem quae nam?
+        </p>
+        <p>
+          Saepe nisi sit odio sapiente obcaecati recusandae perspiciatis eos
+          quis architecto blanditiis dicta quae mollitia culpa, suscipit esse
+          molestiae, dolore veniam fugit ex vero ipsum fugiat hic aperiam. Vero,
+          ullam!
+        </p>
+        <p>
+          Cumque similique unde quos sequi nulla? Placeat aperiam blanditiis
+          doloribus excepturi iste obcaecati voluptatem, optio repellendus
+          quidem sint, assumenda quisquam provident ab dolore ex eaque. Id,
+          repudiandae. Corrupti, adipisci unde.
+        </p>
+      </section>
     </div>
   )
 }
-
 export default App
