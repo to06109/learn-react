1a4df2a82d49192affc407b63266328216b50daa
Yesong Choi <to06109@naver.com>
2023-02-23 15:38:29 +0900

    learn. 컴포넌트 분리 중

--- stateless-component/src/components/Layout/Header.jsx
+++ stateless-component/src/components/Layout/Header.jsx
@@ -1,3 +1,14 @@
+import SkipToContent from '../index'
+
 export function Header() {
-  return <header>앱 헤더</header>
+  return (
+    <header>
+      <h2>앱 헤더</h2>
+      <SkipToContent to="#snack">과자</SkipToContent>
+      <SkipToContent to="#pumpkin">호박</SkipToContent>
+      <SkipToContent to="#icecream">아이스크림</SkipToContent>
+
+      <Nav as="h3" headline="상품 목록" list={navList} />
+    </header>
+  )
 }
